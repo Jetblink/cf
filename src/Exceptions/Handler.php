30c5b92147b5f2243260ba2ed9787bf803e7e388
sh7ning <sh7ning@qq.com>
2016-11-25 13:12:58 +0800

    增加异常日志

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -4,7 +4,16 @@ namespace Tree6bee\Cf\Exceptions;
 
 use Tree6bee\Cf\Contracts\ExceptionHandler as ExceptionHandlerContract;
 use Tree6bee\Support\Helpers\Exceptions\Handler as ExceptionHandler;
+use Tree6bee\Cf\Support\Facades\Log;
 
 class Handler extends ExceptionHandler implements ExceptionHandlerContract
 {
+    /** 
+     * 错误日志记录
+     */
+    protected function report($e)
+    {
+        $content = $this->getLogOfException($e);
+        Log::error($content);
+    }
 }
