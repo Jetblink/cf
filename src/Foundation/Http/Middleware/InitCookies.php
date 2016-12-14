b25f620b13b45e78723c5f2864742947370df203
sh7ning <sh7ning@qq.com>
2016-12-14 17:49:35 +0800

    增加cookie的密钥

--- src/Foundation/Http/Middleware/InitCookies.php
+++ src/Foundation/Http/Middleware/InitCookies.php
@@ -17,7 +17,7 @@ class InitCookies
 
     protected function setCookies($request)
     {
-        $salt = $this->app->config('cookie.salt', 'c!o*o^k#i-e_s%a$l@t');
+        $salt = $this->app->config('cookie_salt', 'c!o*o^k#i-e_s%a$l@t');
         $request->setCookies(new Cookies(new Encrypt($salt)));
 
         return $request;
